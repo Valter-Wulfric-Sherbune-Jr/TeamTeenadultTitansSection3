aa2e25ce182456766532aa1d6a58c9f6b2b8cf74
Mansoor White <mwhite13@ggc.edu>
2017-10-26 16:22:32 -0400

    Add Object Class and Floor 2

--- src/Model/Rooms.java
+++ src/Model/Rooms.java
@@ -34,6 +34,36 @@ class Rooms {
 			room[1][0][4].setDescription("There is a staircase going down one floor and two doors. "
 					+ "The metal safety shields have removed outside light as lights below flicker.");
 			
+			//
+			room[2][2][2].setRoomId(12);
+			room[2][2][2].setName("Second Floor");
+			room[2][2][2].setDescription("This division holds the offices and work centers of employees "
+					+ "who are part of this sectors engineering branch. Large assembly machines can be seen "
+					+ "overhead along with the rail system used for movement. "
+					+ "Black and yellow hazard lines mark which walkways are safe for movement,"
+				+ " and what areas should be avoided.");
+			
+			room[2][1][2].setRoomId(13);
+			room[2][1][2].setName("Second Floor");
+			room[2][1][2].setDescription("The massive concrete and steel-plated room has a large portion of its floorspace "
+					+ "dedicated to the industrial maglev docking station. Freight loading and unloading is "
+					+ "performed in this location by wall and ceiling mounted cranes. "
+					+ "Automated rail carts can be seen stationed with their cargo ready for delivery.");
+			
+			room[2][0][2].setRoomId(14);
+			room[2][0][2].setName("Second Floor");
+			room[2][0][2].setDescription("This location manages and controls the various communications traffic "
+					+ "within the facility. A large screen flanked by multiple smaller ones co-vers the forward-facing "
+					+ "wall. Throughout the room there are stations with multiple monitors used for analyzing"
+					+ " network operations.");
+			
+			room[0][0][0].setRoomId(15);
+			room[0][0][0].setName("Second Floor");
+			room[0][0][0].setDescription("Several elongated hallways are present with numbered residential rooms "
+					+ "positioned on either side. The passages are interconnected by lounging areas that have their "
+					+ "furnishings knocked over. Many of the lighting fixtures that lined the hallways are now "
+					+ "inactive, and have had their functions replaced by the emergency lighting stripes situated at "
+					+ "the hallway corners that now illuminate the narrow passages with amber light.");
 			
 			
 			
