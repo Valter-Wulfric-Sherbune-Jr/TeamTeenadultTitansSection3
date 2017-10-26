c6757f9bb98ceaf9e80e3b919c4de4af1005561a
Mansoor White <mwhite13@ggc.edu>
2017-10-26 15:35:58 -0400

    4th floor Array Added

--- src/Model/Rooms.java
+++ src/Model/Rooms.java
@@ -13,8 +13,26 @@ class Rooms {
 			
 			//Room ArrayList
 			room[0][0][0].setRoomId();
-			room[0][0][0].setName();
+			room[0][0][0].setName("Roof");
 			room[0][0][0].setDescription();
+			
+			//4th Floor 
+			room[0][0][0].setRoomId(01);
+			room[0][0][0].setName();
+			room[0][0][0].setDescription("A breezy Helicopter landing pad with two metal doors. "
+					+ "	The landing pad sits on top of the building "
+					+ "	and the lights of govern-ment vehicles can be seen below.");
+			
+			room[0][1][0].setRoomId(02);
+			room[0][1][0].setName("Roof");
+			room[0][1][0].setDescription("A room with maroon carpet and potted plastic plants in the corner. "
+					+ "here is an elevator with red stains on it. "
+					+ "It doesn’t appear to be functional as the key scanner is broken.");
+			
+			room[1][0][0].setRoomId(03);
+			room[1][0][0].setName("Roof");
+			room[1][0][0].setDescription("There is a staircase going down one floor and two doors. The metal safety shields have removed outside light as lights below flicker.");
+			
 			
 			
 			
