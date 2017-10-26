60d745f9f3b0c745e7ed6597a83dcb6bdcfc12d8
Mansoor White <mwhite13@ggc.edu>
2017-10-26 15:56:54 -0400

    New Room class with Room Object and Getter and setter

--- src/Model/Rooms.java
+++ src/Model/Rooms.java
@@ -17,21 +17,23 @@ class Rooms {
 			room[0][0][0].setDescription();
 			
 			//4th Floor 
-			room[0][0][0].setRoomId(01);
-			room[0][0][0].setName();
-			room[0][0][0].setDescription("A breezy Helicopter landing pad with two metal doors. "
+			room[0][0][4].setRoomId(01);
+			room[0][0][4].setName("Roof");
+			room[0][0][4].setDescription("A breezy Helicopter landing pad with two metal doors. "
 					+ "	The landing pad sits on top of the building "
 					+ "	and the lights of govern-ment vehicles can be seen below.");
 			
-			room[0][1][0].setRoomId(02);
-			room[0][1][0].setName("Roof");
-			room[0][1][0].setDescription("A room with maroon carpet and potted plastic plants in the corner. "
+			room[0][1][4].setRoomId(02);
+			room[0][1][4].setName("Roof");
+			room[0][1][4].setDescription("A room with maroon carpet and potted plastic plants in the corner. "
 					+ "here is an elevator with red stains on it. "
 					+ "It doesn’t appear to be functional as the key scanner is broken.");
 			
-			room[1][0][0].setRoomId(03);
-			room[1][0][0].setName("Roof");
-			room[1][0][0].setDescription("There is a staircase going down one floor and two doors. The metal safety shields have removed outside light as lights below flicker.");
+			room[1][0][4].setRoomId(03);
+			room[1][0][4].setName("Roof");
+			room[1][0][4].setDescription("There is a staircase going down one floor and two doors. "
+					+ "The metal safety shields have removed outside light as lights below flicker.");
+			
 			
 			
 			
@@ -41,6 +43,35 @@ class Rooms {
 	 }
 	
 class Room{
+	public int roomId;
+	public String name;
+	public String description;
+	
+	public Room(int roomId, String name, String description){
+		
+	}
+	public Room(int i, int j, int a, Object object) {
+		// TODO Auto-generated constructor stub
+	}
+	public void setRoomId(int roomId){
+		this.roomId = roomId;
+	}
+	public void setName(String name){
+		this.name=name;
+	}
+	public void setDescription(String description){
+		this.setDescription(description);
+	}
+	
+	public int getRoomId(){
+		return	this.roomId;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public String getDescription(){
+		return this.getDescription();
+	}
 	
 }	 
 	
