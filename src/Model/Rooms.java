114aeb92b4ac4ad53b26a9b886a1b3e2bb902842
Mansoor White <mwhite13@ggc.edu>
2017-10-26 15:13:46 -0400

    Copy and Paste Room and assign the right Array

--- src/Model/Rooms.java
+++ src/Model/Rooms.java
@@ -1,14 +1,23 @@
 package Model;
 import java.util.Arrays;
 
+
 class Rooms {
 	 public static void roomCreate(Room[][][] room,final int x, final int y, final int z){
 		for(int i = 0; i < x; i++){
 			for (int j =0; j < y; j++){
 				for(int a = 0; a< z; a++){
-					room[i][j][a] =new Room;
+					 room[i][j][a] = new Room(i,j,a,null);
 				}
 			}
+			
+			//Room ArrayList
+			room[0][0][0].setRoomId();
+			room[0][0][0].setName();
+			room[0][0][0].setDescription();
+			
+			
+			
 		
 		} 
 	 }
