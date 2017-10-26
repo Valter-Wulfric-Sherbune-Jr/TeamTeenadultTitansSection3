35d597b1a94e0e2d8d9312bd13bd53aa4322c820
Mansoor White <mwhite13@ggc.edu>
2017-10-26 14:59:23 -0400

    Add 3D arraylist

--- src/Model/Rooms.java
+++ src/Model/Rooms.java
@@ -1,5 +1,21 @@
 package Model;
+import java.util.Arrays;
 
-public class Rooms {
+class Rooms {
+	 public static void roomCreate(Room[][][] room,final int x, final int y, final int z){
+		for(int i = 0; i < x; i++){
+			for (int j =0; j < y; j++){
+				for(int a = 0; a< z; a++){
+					room[i][j][a] =new Room;
+				}
+			}
+		
+		} 
+	 }
+	
+class Room{
+	
+}	 
+	
 
 }
