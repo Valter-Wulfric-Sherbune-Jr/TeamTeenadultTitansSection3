1b35577e2bb3a8b201c64a3d1022d4217e47373f
Charibasa <jyu1@ggc.edu>
2017-11-02 14:49:30 -0400

    Menu Commit thing, no idea if this is right

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -24,6 +24,7 @@ import javafx.stage.Stage;
  */
 
 public class Game extends Application{
+	static int menuState;
 	public static Player player;
 	public static String begin;
 	public static Scanner input = new Scanner(System.in);
@@ -36,45 +37,183 @@ public class Game extends Application{
 	
 	public void start(Stage primaryStage)
 	{
-		TextArea consoleTA  = new TextArea("Welcome to the Mock-up Console!");
-		TextArea entryTA    = new TextArea();
+		TextArea consoleTA  = new TextArea("Welcome to the Mock-up Console!\n"
+				+ "\n -- MENU --"
+				+ "\n1. New Game"
+				+ "\n2. Load Game"
+				+ "\n3. Level Editor"
+				+ "\n4. Exit Game");
 		TextField inputTF   = new TextField();
 		
 		VBox rootVBox     = new VBox();
 		
-		rootVBox.getChildren().addAll(consoleTA, entryTA, inputTF);
+		rootVBox.getChildren().addAll(consoleTA, inputTF);
 		rootVBox.setPadding(new Insets(10));
 		
 		primaryStage.setScene(new Scene(rootVBox));
 		primaryStage.setTitle("Console Mock-up");
 		
 		consoleTA.setEditable(false);
+		consoleTA.setWrapText(true);
 		consoleTA.setPrefWidth(1000);
 		consoleTA.setPrefHeight(500);
 		consoleTA.setFont(Font.font("Monospaced"));
-		entryTA.setEditable(false);
-		entryTA.setPrefWidth(1000);
-		entryTA.setPrefHeight(50);
-		entryTA.setFont(Font.font("Monospaced"));
 		inputTF.setPrefWidth(1000);
 		inputTF.setPrefHeight(20);
 		inputTF.setFont(Font.font("Monospaced"));
 		inputTF.requestFocus();
 		
+		
 		inputTF.setOnAction(e -> {
-			if(inputTF.getText().length() >= 90)
+			consoleTA.appendText("\n\n>" + inputTF.getText().toUpperCase());
+			
+			/**
+			 * @author Jason
+			 * The menuState variable keeps track of where the user is in the menus. The states are as follows:
+			 *  menuState = 0: Default state, this is the Main Menu
+			 *  menuState = 1: File creation state, assigned when selecting "New Game"
+			 *  menuState = 2: Load file state, assigned when selecting "Load Game"
+			 *  menuState = 3: Level editor state, assigned when selecting "Level Editor
+			 *  menuState = 4: Level editor v2 state, assigned when selecting "Create Game Folder" in the Level editor state.
+			 *  
+			 *  !!!!! There's probably a better way to implement this, but I dunno how. pls help. !!!!!
+			 */
+			
+			switch(menuState)
 			{
-				entryTA.appendText("\nThat command is too long!");
-				inputTF.clear();
+			case 0:
+				switch(inputTF.getText().toUpperCase().trim())
+				{
+				case "1": case "NEW GAME": case "NEW":
+					consoleTA.appendText("\nNew game? I'll try.");
+					menuState = 1;
+					consoleTA.appendText("\n\nYou're in the New Game menu now.");
+					break;
+				case "2": case "LOAD GAME": case "LOAD":
+					consoleTA.appendText("\nLoad game? I dunno if I can.");
+					menuState = 2;
+					consoleTA.appendText("\n\nYou're in the Load Game menu now.");
+					break;
+				case "3": case "LEVEL EDITOR": case "EDITOR":
+					consoleTA.appendText("\nLevel editor? Why wasn't I told of this?");
+					menuState = 3;
+					consoleTA.appendText("\n\nYou're in the Level Editor menu now.");
+					break;
+				case "4": case "EXIT GAME": case "EXIT":
+					primaryStage.close();
+					break;
+				case "HELP":
+					consoleTA.appendText("\n -- MENU --"
+							+ "\n1. New Game"
+							+ "\n2. Load Game"
+							+ "\n3. Level Editor"
+							+ "\n4. Exit Game");
+					break;
+				default:
+					consoleTA.appendText("\nI don't understand what that means, right now.");
+					break;
+				}
+				break;
+			case 1:
+				switch(inputTF.getText().toUpperCase().trim())
+				{
+				case "1":
+					consoleTA.appendText("\nI don't know what's going on.");
+					break;
+				case "2": case "BACK": case "BACK TO MAIN MENU":
+					consoleTA.appendText("\nBack? Going back to the previous menu...");
+					menuState = 0;
+					consoleTA.appendText("\n\nYou're in the Main Menu now.");
+					break;
+				case "HELP":
+					consoleTA.appendText("\n -- NEW GAME MENU --"
+							+ "\n1. "
+							+ "\n2. Back to Main Menu");
+					break;
+				default:
+					consoleTA.appendText("\nI don't understand what that means, right now.");
+					break;
+				}
+				break;
+			case 2:
+				switch(inputTF.getText().toUpperCase().trim())
+				{
+				case "1":
+					consoleTA.appendText("\nI don't know what's going on.");
+					break;
+				case "2": case "BACK": case "BACK TO MAIN MENU":
+					consoleTA.appendText("\nBack? Going back to the previous menu...");
+					menuState = 0;
+					consoleTA.appendText("\n\nYou're in the Main Menu now.");
+					break;
+				case "HELP":
+					consoleTA.appendText("\n -- LOAD GAME MENU --"
+							+ "\n1. "
+							+ "\n2. Back to Main Menu");
+					break;
+				default:
+					consoleTA.appendText("\nI don't understand what that means, right now.");
+					break;
+				}
+				break;
+			case 3:
+				switch(inputTF.getText().toUpperCase().trim())
+				{
+				case "1": case "CREATE GAME FOLDER": case "CREATE":
+					consoleTA.appendText("\nCreate Game Folder? I dunno what that means but ok");
+					menuState = 4;
+					consoleTA.appendText("\n\nYou're in the Level Editor V2 menu now.");
+					break;
+				case "2":
+					consoleTA.appendText("\nI don't know what's going on.");
+					break;
+				case "3": case "BACK": case "BACK TO MAIN MENU":
+					consoleTA.appendText("\nBack? Going back to the previous menu...");
+					menuState = 0;
+					consoleTA.appendText("\n\nYou're in the Main Menu now.");
+					break;
+				case "HELP":
+					consoleTA.appendText("\n -- LEVEL EDITOR MENU --"
+							+ "\n1. Create Game Folder"
+							+ "\n2. Edit Game Folder"
+							+ "\n3. Back to Main Menu");
+					break;
+				default:
+					consoleTA.appendText("\nI don't understand what that means, right now.");
+					break;
+				}
+				break;
+			case 4:
+				switch(inputTF.getText().toUpperCase().trim())
+				{
+				case "1":
+					consoleTA.appendText("\nLoad Subfolder? ?????");
+					break;
+				case "2":
+					consoleTA.appendText("\nI don't know what's going on.");
+					break;
+				case "3": case "BACK": case "BACK TO LEVEL EDITOR":
+					consoleTA.appendText("\nBack? Going back to the previous menu...");
+					menuState = 3;
+					consoleTA.appendText("\n\nYou're in the Level Editor menu now.");
+					break;
+				case "HELP":
+					consoleTA.appendText("\n -- LEVEL EDITOR V2 MENU --"
+							+ "\n1. Load Subfolder"
+							+ "\n2. ???"
+							+ "\n3. Back to Level Editor");
+					break;
+				default:
+					consoleTA.appendText("\nI don't understand what that means, right now.");
+					break;
+				}
+				break;
 			}
-			else
-			{
-				entryTA.appendText("\n>" + inputTF.getText().toUpperCase());
-				consoleTA.appendText("\n\nYou shout \"" + inputTF.getText().toUpperCase() + "\" out into the void.");
-				consoleTA.appendText("\nNothing interesting happens, and the effort is wasted.");
-				inputTF.clear();
-			}
+			
+			inputTF.clear();
 		});
+		
+		
 		
 		primaryStage.show();
 	}
